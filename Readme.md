22db01105de04eee910d25325d6b9569dc3e80b5
Nabilabilalla <67915843+Nabilabilalla@users.noreply.github.com>
2020-09-27 18:20:44 +0200

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,12 @@
+j'ai commencé étape par étape
+
+j'ai fait le logo en html css, ce qui m'a pris beaucoup de temps
+
+j'ai utilisé beaucoup de position absolute, ce qui m'a contraint de refaire tout le site (temps perdu)
+
+en faisant de nouveau le site, j'ai mis le logo en photo
+
+j'ai fait mon menu en dernier et le responsive après beaucoup de recherches 
+
+j'ai des difficultés dans le JS et PHP, je ne suis pas à l'aise dans ces langages malgré des recherches sur le sujet 
+
---
+++ ev.js
@@ -0,0 +1,59 @@
+
+
+let livres = [
+    {
+        titre: 'kabyle ',
+        auteur: 'sanaaa',
+        image:'',
+        prix: '50',
+        resume: 'la meilleur lange',
+    }
+    ,
+    {
+        titre: 'fransais',
+        auteur: 'mathy',
+        image:'',
+        prix: '40',
+        resume: 'parle mieux fransais'
+    }
+    ,
+    {
+        titre: 'anglais',
+        auteur: 'batmane',
+        image:'',
+        prix: '39',
+        resume: 'tu parles bah barle'
+    }
+    ,
+    {
+        titre: 'arabe',
+        auteur: 'kary',
+        image:'',
+        prix: '10',
+        resume: 'on a bien parles'
+    }
+];
+/* console.log(livres); */
+
+/*function afficheRecommandation() {
+    console.log(livres[0]);
+    console.log(livres[1]);
+    console.log(livres[2])
+}
+afficheRecommandation();*/
+
+/*function afficheLivres(prisMax){
+    return prisMax [(livres.prix <= 50)];
+}*/
+
+livres.pop();
+function afficheRecommandation() {
+    console.log(livres);
+   
+}
+
+
+
+
+        
+   
